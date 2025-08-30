e422efdce06e3f555bcdbc09bd57eb7d6520c4f5
Tomiwa Babatunde <tommybab7@gmail.com>
2025-08-29 19:55:27 -0700

    Removed files from backend folder

--- backend/test_motor.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from motor.motor_asyncio import AsyncIOMotorClient
-
-async def test():
-    client = AsyncIOMotorClient('mongodb://localhost:27017/Atlas2.0')
-    info = await client.server_info()
-    print(info)
-
-asyncio.run(test())
