9559d3bcec33b0ab041f303e4aabe7413ecf40c3
Tomiwa Babatunde <tommybab7@gmail.com>
2026-01-28 04:39:53 -0800

    frontend

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="bg-black/90 py-8 mt-auto text-center text-gray-400 border-t border-blue-500/20">
-    <p>© 2026 Atlas2.0 - All Rights Reserved | Built by Babatunde</p>
+    <p>© 2026 Atlas technologies - All Rights Reserved | Built by Babatunde</p>
   </footer>
 );
 
